f6ae0531ab82e25ff79ea0709eaf0b007e2478e8
liu4715183 <1576456944@qq.com>
2021-05-12 12:27:13 +0800

    第一次上传

---
+++ Name.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+# module typed_ast._ast3
+# from D:\software\python\lib\site-packages\typed_ast\_ast3.cp39-win_amd64.pyd
+# by generator 1.147
+# no doc
+# no imports
+
+from .expr import expr
+
+class Name(expr):
+    # no doc
+    def __init__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    _fields = (
+        'id',
+        'ctx',
+    )
+
+
